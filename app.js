f2a755884db41d788d135e2c06d8ef4d235acbd4
Zscoob <james.zobian@gmail.com>
2019-07-15 23:31:08 -0500

    lab-06

---
+++ app.js
@@ -0,0 +1,30 @@
+// 'use strict'
+// var hours =[0600,0700, 0800,0900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900];
+
+// var pikePlace={
+//     locationName: 'Pike Place Market',
+//      minCustomerPerHour:23,
+//      maxCustomerPerHour:65,
+//      avgCookiesPerSale:6.3,
+//      customersEachHour: [],
+//      cookesEachHour: [],
+//      totalDailyCookeis:'' ,
+//      calcCustomersEachHour: function(){
+//         for (var i = 0; i< hours.length; i++){
+//             this.customersEachHour.push(random(this.minCustomerPerHour,this.maxCustomerPerHour))
+//         }
+//      },
+
+//  //calcCookiesEachHour:function()
+
+ 
+// render(){
+//     var elId, elP;
+//     elId=document.getElementById('wrapper');
+//     elP=document.createElement('p');
+//     elP.textContent='Pikes Place Market';
+//     elId.appendChild(elP);
+// }
+// }
+
+// console.log(pikePlace.render());
